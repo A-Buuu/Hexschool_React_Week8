65a131ecd68edd39edb334b06e77aec323ec8e07
abuchiang <abuuu.chiang@gmail.com>
2025-07-15 10:59:16 +0800

    1. 安裝 sass-embedded 2. 加入版型樣式 3.  匯入 all.scss 及 Bootstrap 的 JS 4. 匯入 font awesome 及字型檔

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// 引入 css
+// 引入 css / JS
 import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/js/bootstrap.js";
 import { RouterProvider } from "react-router-dom";
 
 import router from './router'
+import "./assets/styles/all.scss";
 
 
 createRoot(document.getElementById("root")).render(
